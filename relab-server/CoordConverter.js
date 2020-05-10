4a75d0ee41334558f0212d4f67d3692dd701688b
gianlucabernardotto <bernardotto.gianluca@iisgalvanimi.edu.it>
2020-05-10 13:14:20 +0000

    introduzione api rest

--- relab-server/CoordConverter.js
+++ relab-server/CoordConverter.js
@@ -4,20 +4,23 @@ const Feature = require('./models/feature.model.js');
 const FeatureCollection = require('./models/featureCollection.model.js');
 
 module.exports = class CoordConverter {
+
     constructor()
     {
         //Definisco il tipo di proiezioni da convertire (32632->4362)
         proj4.defs("EPSG:32632", "+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs");
         //proj4.defs("EPSG:4362", "già definito in proj4");
     }
-    //Riceve come parametro il recordset estratto dal DB 
+
     generateGeoJson(recordset) {
-        let geoJsonHeader = new FeatureCollection(); //Crea la Featurecollection
+        let geoJsonHeader = new FeatureCollection();
+
         let i = 0;
         for (const record of recordset) {  
             let polygonGeometry = parse(record[""]); //parso da wkt a geojson geometry
             let geom = this._convertPolygon(polygonGeometry); // converto in "EPSG:4362" 
-            geoJsonHeader.features.push(new Feature(i,geom)); //per ogni poligono nel recordset crea una Feature 
+            // e metto la geometry  geojson
+            geoJsonHeader.features.push(new Feature(i,geom));
         }
         return geoJsonHeader;
     }
